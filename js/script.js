a11eef964c37f919c70c3ebbf67797ca09db0857
Leandro <thesquallrock@gmail.com>
2020-12-16 16:53:49 +0100

    pushino

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Gioco dei dadi
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+var computer, giocatore;
+
+computer = Math.floor(Math.random()*6) + 1;
+console.log(computer);
