172ea786463710f04923aa35c697a012be833be5
Leandro <thesquallrock@gmail.com>
2020-12-16 17:16:29 +0100

    commenti

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// Nome delle variabili
 var computer, giocatore, risultato;
 
+// Click su button "gioca"
 document.getElementById("gioca").addEventListener('click',
 function() {
 // codice
+// definizione variabili
 computer = Math.floor(Math.random()*6) + 1;
 giocatore = Math.floor(Math.random()*6) + 1;
 risultato = 'Pareggio';
@@ -15,8 +18,8 @@ if (computer > giocatore) {
 } else if (giocatore > computer) {
     risultato = 'Hai Vinto'
 }
-console.log(giocatore, computer, risultato);
 
+// valori a schermo
 document.getElementById('ris-computer').innerHTML = computer;
 document.getElementById('ris-giocatore').innerHTML = giocatore;
 document.getElementById('message').innerHTML = risultato;
